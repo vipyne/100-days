66b6746dc5f3d72998c33dfe18d8f72d3ad9eda7
vipyne <vipyne@gmail.com>
2015-05-04 22:29:59 -0400

    clean up

--- 100days/100javascripts.js
+++ 100days/100javascripts.js
@@ -5,32 +5,23 @@
   var daysHTML = document.getElementById('js-days'),
       docFrag  = document.createDocumentFragment();
 
-
-
   for (var i = 1; i <= 10; i++) {
     var ul = document.createElement('ul'), holder = [];
     for (var j = 1*i; j <= 10*i; j++) {
-      console.log('js', j);
-      var lastHTML = [];
-      var HTML;
-      if (j <= 6) {
+      var li = [], HTML;
+      if (j <= 2) {
         HTML = '<li><a href="' + j + '/index.html">day <span>' + j + '</span></a></li>';
       } else {
         HTML = '<li class="is-disabled"><a href="#">day <span>' + j + '</span></a></li>';
       }
       holder.push(HTML);
-      lastHTML = holder.slice(-10);
-      // var ten = holder.pop(10);
-      ul.innerHTML = lastHTML.join('');
+      li = holder.slice(-10);
+      ul.innerHTML = li.join('');
     }
-    // console.log(holder);
-    console.log('docFrag', docFrag);
     docFrag.appendChild(ul);
     ul = null;
   }
 
-  console.log('doc', docFrag);
   daysHTML.appendChild(docFrag);
-  // daysHTML.innerHTML = docFrag;
 
 })(window, document);
