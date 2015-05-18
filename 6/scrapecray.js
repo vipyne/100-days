3749bd179326e0df71f71ca626421e4081200e9e
vipyne <vipyne@gmail.com>
2015-05-17 23:05:05 -0400

    messing with scraper module

--- 6/scrapecray.js
+++ 6/scrapecray.js
@@ -1,9 +1,22 @@
-  var scraperjs = require('scraperjs');
-  scraperjs.DynamicScraper.create('https://news.ycombinator.com/')
-    .scrape(function() {
-        return $(".title a").map(function() {
-            return $(this).text();
-        }).get();
-    }, function(news) {
-        console.log(news);
-    })
+  // var scraperjs = require('scraperjs');
+  // scraperjs.DynamicScraper.create('https://news.ycombinator.com/')
+  //   .scrape(function() {
+  //       return $(".title a").map(function() {
+  //           return $(this).text();
+  //       }).get();
+  //   }, function(news) {
+  //       console.log(news);
+  //   })
+
+
+var http =  require('http'),
+    https = require('https'),
+    url = require('url'),
+    Scraper = require('https://github.com/vipyne/scraper/tarball/master');
+var address = 'https://www.google.com/search?q=cray+supercomputer&biw=1361&bih=802&source=lnms&tbm=isch&sa=X&ei=_k5ZVfz5HcGqsAWRh4HQAw&sqi=2&ved=0CAcQ_AUoAg';
+// var address = 'https://www.reddit.com';
+// var address = 'https://www.cray.com';
+var scraper = new Scraper.Scraper();
+scraper.scrape(address).then(function (thumbs) {
+    console.log(thumbs);
+});
