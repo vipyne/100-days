92f5f2f99a2b9a73134c05b0aef0cf866968ad5c
vipyne <vipyne@gmail.com>
2015-05-19 21:47:58 -0400

    adjusting filters

--- 6/scrapecray.js
+++ 6/scrapecray.js
@@ -12,7 +12,7 @@
 var http =  require('http'),
     https = require('https'),
     url = require('url'),
-    Scraper = require('https://github.com/vipyne/scraper/tarball/master');
+    Scraper = require('scraper-js');
 var address = 'https://www.google.com/search?q=cray+supercomputer&biw=1361&bih=802&source=lnms&tbm=isch&sa=X&ei=_k5ZVfz5HcGqsAWRh4HQAw&sqi=2&ved=0CAcQ_AUoAg';
 // var address = 'https://www.reddit.com';
 // var address = 'https://www.cray.com';
