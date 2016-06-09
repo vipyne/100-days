6baa9547728be9ad49a3cc6bcf58d9fa5269a75f
vipyne <vipyne@gmail.com>
2016-06-08 23:07:17 -0400

    dumb sorting of array for now

--- 7/js/imagedata.js
+++ 7/js/imagedata.js
@@ -16,20 +16,26 @@
 
   imageContext.drawImage(orginalImage, 0, 0)
   var imageData = imageContext.getImageData(0, 0, width, height);
-  var rgba_byte_array = imageData.data;
+  var rgbaByteArray = imageData.data;
+
+  var qwer = new Uint8ClampedArray;
+  var sillysort = rgbaByteArray.sort(function(a, b) {
+    return a - b
+  });
+
 
   var red = 0,
       green = 0,
       blue = 0;
-  for (var i = 0; i < rgba_byte_array.length/10022; i++) {
+  for (var i = 0; i < rgbaByteArray.length/10000; i++) {
 
     var index = i * 4;
     // red
-      red += rgba_byte_array[index];
+      red += Math.floor(rgbaByteArray[index]/100);
     // green
-      green += rgba_byte_array[index + 1];
+      green += Math.floor(rgbaByteArray[index + 1]/100);
     // blue
-      blue += rgba_byte_array[index + 2];
+      blue += Math.floor(rgbaByteArray[index + 2]/100);
 
     console.log('poop')
     console.log('red', red)
@@ -43,26 +49,29 @@
 
   console.log(biggest % height )
 
-  canvasContext.beginPath();
-  canvasContext.moveTo(0, 0);
-  canvasContext.lineTo(0, height);
-  canvasContext.strokeStyle = '#c0f';
-  canvasContext.lineWidth = canvas.getAttribute('width') / 3 * 2;
-  canvasContext.stroke();
+  // canvasContext.beginPath();
+  // canvasContext.moveTo(0, 0);
+  // canvasContext.lineTo(0, height);
+  // canvasContext.strokeStyle = '#c0f';
+  // canvasContext.lineWidth = canvas.getAttribute('width') / 3 * 2;
+  // canvasContext.stroke();
 
-  canvasContext.beginPath();
-  canvasContext.moveTo(width/2, 0);
-  canvasContext.lineTo(width/2, height-50);
-  canvasContext.strokeStyle = '#2c005f';
-  canvasContext.lineWidth = canvas.getAttribute('width') / 3;
-  canvasContext.stroke();
+  // canvasContext.beginPath();
+  // canvasContext.moveTo(width/2, 0);
+  // canvasContext.lineTo(width/2, height-50);
+  // canvasContext.strokeStyle = '#2c005f';
+  // canvasContext.lineWidth = canvas.getAttribute('width') / 3;
+  // canvasContext.stroke();
 
-  canvasContext.beginPath();
-  canvasContext.moveTo(width/3*2.5, 0);
-  canvasContext.lineTo(width/3*2.5, height-100);
-  canvasContext.strokeStyle = '#0c3f5f';
-  canvasContext.lineWidth = canvas.getAttribute('width') / 3;
-  canvasContext.stroke();
+  // canvasContext.beginPath();
+  // canvasContext.moveTo(width/3*2.5, 0);
+  // canvasContext.lineTo(width/3*2.5, height-100);
+  // canvasContext.strokeStyle = '#0c3f5f';
+  // canvasContext.lineWidth = canvas.getAttribute('width') / 3;
+  // canvasContext.stroke();
+
+  var imgData = new ImageData(sillysort,300);
+  canvasContext.putImageData(imgData, 0, 0)
 
   console.log('height', height)
   console.log('width', width)
