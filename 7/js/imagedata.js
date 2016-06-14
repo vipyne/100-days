ba2d01dd9f45d2516de76ecfa968be0488b3d107
vipyne <vipyne@gmail.com>
2016-06-14 00:01:41 -0400

    light to dark

--- 7/js/imagedata.js
+++ 7/js/imagedata.js
@@ -1,8 +1,8 @@
 ;(function(w, d, u) {
 
   var orginalImage = document.getElementById('original-img'),
-      height = orginalImage.getAttribute('height')/2,
-      width = orginalImage.getAttribute('width')/2,
+      height = orginalImage.getAttribute('height'),
+      width = orginalImage.getAttribute('width'),
       imgCanvas = document.getElementById('img-canvas'),
       canvas = document.getElementById('draw-canvas');
 
@@ -18,36 +18,36 @@
   var imageData = imageContext.getImageData(0, 0, width, height);
   var rgbaByteArray = imageData.data;
 
-  // var sillysort = rgbaByteArray.sort(function(a, b) {
-  //   return a - b
-  // });
-
   // array to hold an array of the RGBA values
   var twoDArr = [];
 
   var red = 0,
       green = 0,
       blue = 0;
-  for (var i = 0; i < rgbaByteArray.length; i++) {
+  for (var i = 0; i < rgbaByteArray.length; i+=4) {
 
     var index = i;
 
-    twoDArr[index] = [rgbaByteArray[index],
-                      rgbaByteArray[index+1],
-                      rgbaByteArray[index+2],
-                      rgbaByteArray[index+3]]
+    twoDArr[i] = [rgbaByteArray[index],
+                  rgbaByteArray[index+1],
+                  rgbaByteArray[index+2],
+                  rgbaByteArray[index+3]]
   }
 
   // array to hold raw RGBA values... essentially flatten twoDArr
   var tempArr = [];
 
-  // sort from largest red value to smallest
+  // sort from smallest to largest green value
   var sillysortpixels = twoDArr.sort(function(a, b) {
-    return (b[0] + b[1] + b[2]) - (a[0] + a[1] + a[2]);
+    if (a[0] !== undefined && b[0] !== undefined) {
+      if (a[1] < b[1]) return -1;
+      if (b[1] < a[1]) return 1;
+      return 0;
+    }
   });
 
   for (var j = 0; j < sillysortpixels.length; j++) {
-    if (sillysortpixels[j]) {
+    if (sillysortpixels[j] && sillysortpixels[j][0] !== undefined) {
       tempArr.push(sillysortpixels[j][0]);
       tempArr.push(sillysortpixels[j][1]);
       tempArr.push(sillysortpixels[j][2]);
@@ -58,7 +58,7 @@
   var sillysort = new Uint8ClampedArray(tempArr.length);
   sillysort.set(tempArr);
 
-  var imgData = new ImageData(sillysort,300);
+  var imgData = new ImageData(sillysort, width);
   canvasContext.putImageData(imgData, 0, 0)
 
   console.log('EOF')
