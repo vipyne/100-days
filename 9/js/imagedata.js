dc8e3f85e631f936fcbbaa4bf0afe13cc6222b05
vipyne <vipyne@gmail.com>
2016-08-25 06:46:16 -0400

    who knows what i was in the middle of...

--- 9/js/imagedata.js
+++ 9/js/imagedata.js
@@ -16,10 +16,12 @@
       imageData,
       rgbaByteArray,
       twoDArr = [], // hold RGBA value arrays
+      justTwoDArr = [], // hold RGBA value arrays
       tempArrOne = [], // hold raw RGBA values... essentially flatten twoDArr
       tempArrTwo = [], // hold raw RGBA values... essentially flatten twoDArr
       sillySortPixels,
       sillySortPixelsGreen,
+      sillySortPixelsJustGreen = [],
       sillySortPixelsRed,
       sillySortPixelsBlue,
       sillysort,
@@ -43,54 +45,59 @@
   // create 2D array from RGBA byte array
   for (var i = 0; i < rgbaByteArray.length; i += 4) {
     var index = i;
+    // twoDArr[i] = [0, rgbaByteArray[index + 1], rgbaByteArray[index + 2], 255]
     twoDArr[i] = [rgbaByteArray[index],
                   rgbaByteArray[index + 1],
                   rgbaByteArray[index + 2],
-                  rgbaByteArray[index + 3]]
+                  255]
+    justTwoDArr[i] = [rgbaByteArray[index],
+                  rgbaByteArray[index + 1],
+                  rgbaByteArray[index + 2],
+                  255]
   }
-
   console.log('a', rgbaByteArray[10])
+
+  sillySortPixelsJustGreen = justTwoDArr.sort(function(a, b) {
+    // debugger;
+    // if (a[color.GREEN] < b[color.GREEN]) return -1;
+    // if (b[color.GREEN] < a[color.GREEN]) return 1;
+    return 0;
+  });
 
   // sort from smallest to largest green value
   sillySortPixelsGreen = twoDArr.sort(function(a, b) {
-    if (a[0] !== undefined && b[0] !== undefined) {
-      if (a[color.GREEN] < b[color.GREEN]) return -1;
-      if (b[color.GREEN] < a[color.GREEN]) return 1;
+    if (a[color.GREEN] < b[color.GREEN]) return -1;
+    if (b[color.GREEN] < a[color.GREEN]) return 1;
+    return 0;
+  });
+  console.log('green', sillySortPixelsGreen[10])
+  console.log('eq?', sillySortPixelsJustGreen === sillySortPixelsBlue)
+
+  // sort from smallest to largest blue value
+  sillySortPixelsBlue = sillySortPixelsGreen.sort(function(a, b) {
+    // if (a[color.GREEN] === b[color.GREEN]) {
+      // console.log('blue', a[color.GREEN], b[color.GREEN])
+      if (a[color.GREEN] === b[color.GREEN] && a[color.BLUE] < b[color.BLUE]) return -1;
+      if (a[color.GREEN] === b[color.GREEN] && b[color.BLUE] < a[color.BLUE]) return 1;
+      return 0;
+    // } else {
+    //   return 0;
+    // }
+  });
+  console.log('blue', sillySortPixelsBlue[10])
+
+  // sort from smallest to largest red value
+  sillySortPixelsRed = sillySortPixelsBlue.sort(function(a, b) {
+    if (a[color.RED] === b[color.RED] && a[color.GREEN] === b[color.GREEN]) {
+      console.log('red')
+      if (a[color.RED] < b[color.RED]) return -1;
+      if (b[color.RED] < a[color.RED]) return 1;
+      return 0;
+    } else {
       return 0;
     }
   });
-  console.log('green', sillySortPixelsGreen[10])
-
-  // sort from smallest to largest red value
-  sillySortPixelsRed = sillySortPixelsGreen.sort(function(a, b) {
-    if (a[0] !== undefined && b[0] !== undefined) {
-      if (a[color.GREEN] === b[color.GREEN]) {
-        if (a[color.RED] < b[color.RED]) return -1;
-        if (b[color.RED] < a[color.RED]) return 1;
-        return 0;
-      } else {
-        return 0;
-      }
-    }
-  }); 
   console.log('red', sillySortPixelsRed[10])
-
-  // sort from smallest to largest blue value
-  sillySortPixelsBlue = sillySortPixelsRed.sort(function(a, b) {
-    if (a[0] !== undefined && b[0] !== undefined) {
-      if (a[color.RED] === b[color.RED] && a[color.GREEN] === b[color.GREEN]) {
-        if (a[color.BLUE] < b[color.BLUE]) return -1;
-        if (b[color.BLUE] < a[color.BLUE]) return 1;
-        return 0;
-      } else {
-        return 0;
-      }
-    }
-  }); 
-
-  console.log('blue', sillySortPixelsBlue[10])
-  // sillySortPixels = sillySortPixelsGreen;
-  // sillySortPixels = sillySortPixelsBlue;
 
   function flatten(sillySortPixels, tempArr) {
     // create flat array of sorted RGBA values
@@ -104,7 +111,9 @@
     }
   }
 
-  flatten(sillySortPixelsGreen, tempArrOne);
+  console.log('eq?', sillySortPixelsJustGreen[100] === sillySortPixelsBlue[100])
+
+  flatten(sillySortPixelsJustGreen, tempArrOne);
   flatten(sillySortPixelsBlue, tempArrTwo);
 
   sillysort = new Uint8ClampedArray(tempArrOne.length);
