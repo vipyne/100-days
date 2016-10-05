ff6a24eb59a41037e4eb350524878f542b8359f9
vipyne <vipyne@gmail.com>
2016-10-04 23:15:58 -0400

    updating rainbow... going to work out of this directory for now

--- 11/rainbow/animate_the_rainbow-copy.sh
+++ 11/rainbow/animate_the_rainbow-copy.sh
@@ -6,20 +6,23 @@ loops="$1"
 
 for i in $(ls rainbow_frames/1_intro); do
   xattr -wx com.apple.metadata:_kMDItemUserTags \ "`xattr -px com.apple.metadata:_kMDItemUserTags ./rainbow_frames/1_intro/$i`" animated_rainbow;
-  rm -rf untitled*
-  osascript -e 'tell application "Finder" to delete (make new folder at (front window) with properties {name:"_"})' > ./unused/err.txt;
+  # rm -rf _*
+  osascript -e 'tell application "Finder" to delete (make new folder at (front window))' > ./rainbow_frames/unused/err.txt;
+  # osascript -e 'tell application "Finder" to delete (make new folder at (front window) with properties {name:"_"})' > ./rainbow_frames/unused/err.txt;
 done
 
 for ((n = 0; n < $loops; n++)); do
   for i in $(ls rainbow_frames/2_loop); do
     xattr -wx com.apple.metadata:_kMDItemUserTags \ "`xattr -px com.apple.metadata:_kMDItemUserTags ./rainbow_frames/2_loop/$i`" animated_rainbow;
-    rm -rf untitled*
-    osascript -e 'tell application "Finder" to delete (make new folder at (front window) with properties {name:"_"})' > ./unused/err.txt;
+    # rm -rf _*
+  osascript -e 'tell application "Finder" to delete (make new folder at (front window))' > ./rainbow_frames/unused/err.txt;
+    # osascript -e 'tell application "Finder" to delete (make new folder at (front window) with properties {name:"_"})' > ./rainbow_frames/unused/err.txt;
   done
 done
 
 for i in $(ls rainbow_frames/3_outro); do
   xattr -wx com.apple.metadata:_kMDItemUserTags \ "`xattr -px com.apple.metadata:_kMDItemUserTags ./rainbow_frames/3_outro/$i`" animated_rainbow;
-  rm -rf untitled*
-  osascript -e 'tell application "Finder" to delete (make new folder at (front window) with properties {name:"_"})' > ./unused/err.txt;
+  # rm -rf _*
+  osascript -e 'tell application "Finder" to delete (make new folder at (front window))' > ./rainbow_frames/unused/err.txt;
+  # osascript -e 'tell application "Finder" to delete (make new folder at (front window) with properties {name:"_"})' > ./rainbow_frames/unused/err.txt;
 done
