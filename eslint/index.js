34ec51d815019657475de1cf21f45f320c52ec4c
vipyne <vipyne@gmail.com>
2018-06-12 18:25:37 -0400

    testing some eslint thing

---
+++ eslint/index.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: "airbnb/base",
+  rules: {
+    semi: "off",
+    "no-plusplus": "off",
+    "no-console": "warn",
+    "no-magic-numbers": "off",
+    "no-unused-vars": "error",
+    "comma-dangle": "off",
+    "eol-last": "error",
+    indent: [ "error", 2 ],
+    "multiline-comment-style": [ "warn", "starred-block" ],
+    "no-inline-comments": "error",
+    eqeqeq: "warn",
+    camelcase: "warn",
+    "no-param-reassign": "warn",
+    "no-use-before-define": "off",
+    "no-restricted-syntax": "off",
+    "guard-for-in": "off",
+    "no-underscore-dangle": "off",
+    "yoda": "off"
+  }
+}
