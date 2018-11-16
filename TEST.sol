6a21bf05bd5cb2c3ed1a561feba6224f45148157
Vanessa Pyne <vipyne@gmail.com>
2018-11-16 11:24:27 -0500

    Update TEST.sol

--- TEST.sol
+++ TEST.sol
@@ -1,1 +1,20 @@
 pragma solidity ^0.4.24;
+
+/// https://github.com/cryptofinlabs/bskt/blob/master/contracts/BsktToken.sol
+/// just testing syntax highlighting
+library AddressArrayUtils {
+
+    /// @return Returns index and ok for the first occurrence starting from
+    /// index 0
+    function index(address[] addresses, address a)
+        internal pure returns (uint, bool)
+    {
+        for (uint i = 0; i < addresses.length; i++) {
+            if (addresses[i] == a) {
+                return (i, true);
+            }
+        }
+        return (0, false);
+    }
+
+}
